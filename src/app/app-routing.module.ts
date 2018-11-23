b73a4ccfc275e9f0a7bb7e8d56aadb3430cd1513
Kushan <kaavinda92@gmail.com>
2018-11-23 23:26:15 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ViewComponent } from './view/view.component';
 
-const routes: Routes = [{path: 'view', component: ViewComponent},{path: 'retrive', component: ViewComponent}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegFormComponent } from './reg-form/reg-form.component';
-import { ViewFormComponent } from './view-form/view-form.component';
-import { Retrive1Component } from './retrive1/retrive1.component';
-import { NewcomersComponent } from './newcomers/newcomers.component';
-import { AddCustomerInquiryComponent } from './add-customer-inquiry/add-customer-inquiry.component';
-import { ViewComponent } from './view/view.component';
 import { FormsModule } from '@angular/forms';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Form2Component } from './form2/form2.component';
@@ -16,12 +10,6 @@ import { Form2Component } from './form2/form2.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RegFormComponent,
-    ViewFormComponent,
-    Retrive1Component,
-    NewcomersComponent,
-    AddCustomerInquiryComponent,
-    ViewComponent,
     NavBarComponent,
     Form2Component
   ],
